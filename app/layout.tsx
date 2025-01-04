c1fc76fa6aac033f36a3ebb7f552e1ab839bcc8e
chahalamol <51814586+chahalamol@users.noreply.github.com>
2025-01-04 14:07:41 -0800

    Update layout metadata and remove help, privacy, and terms pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Metadata, Viewport } from 'next';
 import { CartProvider } from '@/components/cart-context';
 
 export const metadata: Metadata = {
-  title: 'NEXYZY',
-  description: 'Inspired by yeezy.com, built with Next.js.',
+  title: 'vinyls',
+  description: 'a collection of vinyls',
 };
 
 export const viewport: Viewport = {
